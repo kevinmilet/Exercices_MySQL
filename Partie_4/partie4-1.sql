8106c8955d8f8bb1443209d904a2645a1fd5e5e1
kevinmilet <kevin.milet@gmail.com>
2021-01-25 14:28:26 +0100

    partie 4 ok

---
+++ Partie_4/partie4-1.sql
@@ -0,0 +1,12 @@
+USE `webDevelopment`;
+
+INSERT INTO `languages` (`language`, `version`)
+    VALUES
+    ('JavaScript', 'version 5'),
+    ('PHP', 'version 5.2'),
+    ('PHP', 'version 5.4'),
+    ('HTML', 'version 5.1'),
+    ('JavaScript', 'version 6'),
+    ('JavaScript', 'version 7'),
+    ('JavaScript', 'version 8'),
+    ('PHP', 'version 7');
---
+++ Partie_4/partie4-2.sql
@@ -0,0 +1,8 @@
+USE `webDevelopment`;
+
+INSERT INTO `frameworks` (`framework`, `version`)
+    VALUES
+    ('Symfony', 'version 2.8'),
+    ('Symfony', 'version 3'),
+    ('JQuery', 'version 1.6'),
+    ('JQuery', 'version 2.10');
