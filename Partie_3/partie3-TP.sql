0cb9a3fe2fa35bacc38d6c7caaad1a3e8ef879b4
kevinmilet <kevin.milet@gmail.com>
2021-01-26 12:18:32 +0100

    ...

--- Partie_3/partie3-TP.sql
+++ Partie_3/partie3-TP.sql
@@ -1,3 +1,10 @@
+-- Dans la base codex, dans la table clients :
+
+-- supprimer la colonne secondPhoneNumber
+-- renommer la colonne firstPhoneNumber en phoneNumber
+-- changer le type de la colonne phoneNumber en VARCHAR
+-- ajouter les colonnes zipCode(VARCHAR) et city(VARCHAR)
+
 USE `codex`;
 
 ALTER TABLE `clients` DROP COLUMN `secondPhoneNumber`;
@@ -7,5 +14,3 @@ ALTER TABLE `clients` CHANGE `firstPhoneNumber` `phoneNumber` VARCHAR(30);
 ALTER TABLE `clients` ADD COLUMN `zipCode` VARCHAR(10);
 
 ALTER TABLE `clients` ADD COLUMN `city` VARCHAR(30);
-
-
