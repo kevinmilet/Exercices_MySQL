e02ac5686fdf40e2c1d0c2017201b84092eeae2d
kevinmilet <kevin.milet@gmail.com>
2021-01-26 16:56:04 +0100

    ...

--- Bonus/bonus.sql
+++ Bonus/bonus.sql
@@ -1,3 +1,20 @@
 -- BONUS CLES ETRANGERES
 -- Créer une bdd de clients (nom, prénom, ville de résidence) pour gérer leurs commandes.
 
+DROP DATABASE IF EXISTS `clients`;
+
+CREATE DATABASE `clients`;
+
+CREATE TABLE `clients` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `lastname` VARCHAR(30) NOT NULL,
+    `firstname` VARCHAR(30) NOT NULL,
+    `city` INT NOT NULL
+);
+
+CREATE TABLE `cities`(
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `city` VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE `commands`;
