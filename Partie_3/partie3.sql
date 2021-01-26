7d42b96ab8755197e9d3b3103279923e824b6562
kevinmilet <kevin.milet@gmail.com>
2021-01-26 12:03:56 +0100

    fichiers partie 8 + ajout commentaires partie 3

--- Partie_3/partie3.sql
+++ Partie_3/partie3.sql
@@ -1,11 +1,26 @@
 USE `webDevelopment`;
 
+-- Exercice 1
+-- Dans la base de données webDevelopment, ajouter à la table languages une colonne versions (VARCHAR).
+
 ALTER TABLE `languages` ADD COLUMN `versions` VARCHAR(30);
+
+-- Exercice 2
+-- Dans la base de données webDevelopment, ajouter à la table frameworks une colonne version (INT).
 
 ALTER TABLE `frameworks` ADD COLUMN `version` INT;
 
+-- Exercice 3
+-- Dans la base de données webDevelopment, dans la table languages renommer la colonne versions en version.
+
 ALTER TABLE `languages` CHANGE `versions` `version` VARCHAR(30);
 
+-- Exercice 4
+-- Dans la base de données webDevelopment, dans la table frameworks, renommer la colonne name en framework.
+
 ALTER TABLE `frameworks` CHANGE `name` `framework` VARCHAR(30);
+
+-- Exercice 5
+-- Dans la base de données webDevelopment, dans la table frameworks changer le type de la colonne version en VARCHAR de taille 10.
 
 ALTER TABLE `frameworks` MODIFY `version` VARCHAR(10);
---
+++ Partie_8/Partie_8.sql
@@ -0,0 +1,3 @@
+-- Exercice 1
+-- Afficher tous les frameworks associés à leurs langages. Si un langage n'a pas de framework l'afficher aussi.
+
