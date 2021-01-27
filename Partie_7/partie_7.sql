bc998867c2b27dd72112be85cb6d2dabc2fc28be
kevinmilet <kevin.milet@gmail.com>
2021-01-27 13:02:19 +0100

    ...

--- Partie_7/partie_7.sql
+++ Partie_7/partie_7.sql
@@ -1,7 +1,7 @@
 -- Exercicec 1
 -- Dans la table languages, supprimer toutes les lignes parlant de HTML.
 
-DELETE FROM `languages` where `language` = 'HTML';
+DELETE FROM `languages` WHERE `language` = 'HTML';
 
 -- Exercice 2
 -- Dans la table frameworks, modifier toutes les lignes ayant le framework Symfony par Symfony2.
@@ -11,4 +11,4 @@ UPDATE `frameworks` SET `framework` = 'Symfony2' WHERE `framework` = 'Symfony';
 -- Exercice 3
 -- Dans la table languages, modifier la ligne du langage JavaScript version 5 par la version 5.1
 
-UPDATE `languages` SET `version` = '5.1' WHERE `language` = 'Javascript' AND `version` = '5';
+UPDATE `languages` SET `version` = 'Version 5.1' WHERE `language` = 'Javascript' AND `version` = 'Version 5';
